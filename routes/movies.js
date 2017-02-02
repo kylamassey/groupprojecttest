d786e0fb44c0bcb9c84b11d30f3c1b4ef26e8a90
Tenzin Chhosphel <tenchhos@gmail.com>
2017-02-01 21:16:21 -0500

    login required before seeing movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index');
+var authHelpers = require('../auth/auth-helpers');
+
 /* GET movies page. */
 
 // creates route to display all movies in movies database on the dom.
-router.get('/', function(req, res, next) {
+router.get('/', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.findAll({}).then(function(movies) {
     res.render('movies', {
       title: 'Movies',
@@ -14,12 +16,12 @@ router.get('/', function(req, res, next) {
 });
 
 // creates a page that allows user to add a movie to the database
-router.get('/new', function(req, res, next) {
+router.get('/new', authHelpers.loginRequired, function(req, res, next) {
   res.render('movies/new');
   // res.send(newMovieContent);
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.create({
     title: req.body.title,
     synopsis: req.body.synopsis,
@@ -30,7 +32,7 @@ router.post('/', function(req, res, next) {
 });
 
 // creates route to movies/id# that renders movie titles and synopsis based on whichever movie id was requested
-router.get('/:id', function(req, res, next) {
+router.get('/:id', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.findById(req.params.id).then(function(movies) {
     res.render('moviesHome', {
       title: movies.title,
@@ -41,7 +43,7 @@ router.get('/:id', function(req, res, next) {
 });
 
 // GET /movies/:id/edit: this should bring the user to a form to edit the info. of the movie corresponding to the id. Don't worry about allowing the user to edit the director for now, we can't be sure that whomever is in charge of that part of the app has completed their work
-router.get('/:id/edit', function(req, res, next) {
+router.get('/:id/edit', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.findById(req.params.id).then(function(movie) {
     res.render('moviesEdit', {
       movie:movie
@@ -50,7 +52,7 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 // posts edited movie info submitted (updates) don't forget method override npm install and then add to app.js
-router.put('/:id', function(req, res, next) {
+router.put('/:id', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.update({
     title: req.body.title,
     synopsis: req.body.synopsis
@@ -60,7 +62,7 @@ router.put('/:id', function(req, res, next) {
   });
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', authHelpers.loginRequired, function(req, res, next) {
   models.Movie.destroy({
     where: { id: req.params.id }
   }).then(function(movie) {
