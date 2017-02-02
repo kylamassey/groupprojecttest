d80500f43120c1394702213ff4ba34b00f0aaf73
Tenzin Chhosphel <tenchhos@gmail.com>
2017-02-01 21:19:53 -0500

    login requried for directors

--- routes/directors.js
+++ routes/directors.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index');
+var authHelpers = require('../auth/auth-helpers');
 
 /* GET directors page. */
 // router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ var models = require('../db/models/index');
 // });
 
 //RENDER inside directors the names
-router.get('/', function(req, res, next) {
+router.get('/', authHelpers.loginRequired, function(req, res, next) {
   models.Director.findAll({}).then(function(directors) {
     res.render('directors', {
        title: 'Directors',
@@ -18,19 +19,19 @@ router.get('/', function(req, res, next) {
  });
 
  /* GET new listing. */
- router.get('/new', function(req, res, next) {
+ router.get('/new', authHelpers.loginRequired, function(req, res, next) {
    res.render('newDirector', { title: 'new' });
  });
 
 //SELECT
 // Render the name of the director id
-router.get('/:id', function(req, res, next) {
+router.get('/:id', authHelpers.loginRequired, function(req, res, next) {
   models.Director.findById(req.params.id).then(function(directors) {
     res.render('show', { directors: directors });
   });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', authHelpers.loginRequired, function(req, res, next) {
   models.Director.create({
     name: req.body.name,
   }).then(function() {
@@ -39,13 +40,13 @@ router.post('/', function(req, res, next) {
 });
 
 //EDIT
-router.get('/:id/edit', function(req, res, next) {
+router.get('/:id/edit', authHelpers.loginRequired, function(req, res, next) {
   models.Director.findById(req.params.id).then(function(directors) {
     res.render('edit', { directors: directors });
   });
 });
 
-router.put('/:id', function(req, res, next) {
+router.put('/:id', authHelpers.loginRequired, function(req, res, next) {
    models.Director.update({
        name: req.body.name
    }, { where: { id: req.params.id } }).then(function() {
